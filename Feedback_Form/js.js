dead9aa812d937a73c0215b1d58707060ec93b8b
snowy_lone_wolf <57251093+iamtatha@users.noreply.github.com>
2025-08-28 21:31:11 +0530

    Delete Feedback_Form directory

--- Feedback_Form/js.js
+++
@@ -1,9 +0,0 @@
-function Caution(){
-	var x=document.getElementById('caution');
-	x.style.display='inline-block';
-}
-
-function Hide(){
-	var x=document.getElementById('caution');
-	x.style.display='none';
-}
